2b2c4bef7bf62e29f9b379eccdb5dc6df88085b0
QUang Huy <huynq23091@gmail.com>
2024-10-07 04:37:24 -0400

    Update hello_user.c

--- user_space/src/hello_user.c
+++ user_space/src/hello_user.c
@@ -1,24 +1,7 @@
 // hello_user.c
-#include "syscall_user.h"
 #include <stdio.h>
 
-
-void main() {
-    pid_t pid = fork();
-
-    if (pid < 0) {
-        // Lỗi khi gọi fork
-        printf("Fork failed\n");
-        return 1;
-    }
-
-    if (pid == 0) {
-        // Đây là tiến trình con
-        printf("Hello from child process! PID: %ld\n", (long)getpid());
-    } else {
-        // Đây là tiến trình cha
-        printf("Hello from parent process! Child PID: %ld\n", (long)pid);
-    }
-
+int main() {
+    printf("Hello from user space!\n");
     return 0;
 }
